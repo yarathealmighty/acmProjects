380e0fdb8830e1201dee23eab6483b56e9fa0472
h268081 <h268081@stud.u-szeged.hu>
2023-11-14 19:04:57 +0100

    primitive confinement approach for 2-CNF

--- giantPizza/main.cpp
+++ giantPizza/main.cpp
@@ -5,11 +5,49 @@
 
 using namespace std;
 
-vector<Clause> sigma;
+vector<Clause> sigmaO;
 
-vector<char> confine(vector<Clause> sigma){
-    vector<char> output;
-    //todo confine
+vector<int> confine(vector<Clause> sigma){
+    vector<int> output;
+    vector<Clause> newSigma;
+    int l;
+    if(sigma[0].first==0){
+        l=sigma[0].second;
+    } else {
+        l = sigma[0].first;
+    }
+    output.push_back(l);
+    for(auto itr: sigma){
+        Clause newClause = itr;
+        //take out the clause from the sigma
+        if(l==itr.first || l==itr.second){
+            continue;
+        }
+        //take out the number from the clause
+        if(l*-1==itr.first){
+            newClause.first=0;
+            newClause.second=itr.second;
+        } else {
+            if(l*-1==itr.second){
+                newClause.first=itr.first;
+                newClause.second=0;
+            }
+        }
+        //take clause out entirely if it's empty
+        //todo this indicates impossibility
+        if(newClause.first==0 && newClause.second==0){
+            continue;
+        }
+        newSigma.push_back(newClause);
+    }
+
+    //repeat until sigma is empty
+    if(!newSigma.empty()){
+        vector<int> newAdd = confine(newSigma);
+        for(int i=0;i<newAdd.size();i++){
+            output.push_back(newAdd[i]);
+        }
+    }
     return output;
 }
 
@@ -26,13 +64,23 @@ int main(){
         if(tmpy=='-'){
             y*=-1;
         }
-        sigma.push_back(make_pair(x,y));
+        sigmaO.push_back(make_pair(x,y));
     }
-    vector<char> output = confine(sigma);
+    vector<int> output = confine(sigmaO);
 
     if(possible) {
-        for (auto itr: output) {
-            cout << itr << " ";
+        int counter=0;
+        for(int i=1;i<=m;i++){
+            if(i==counter){
+                if(output[i]>0){
+                    cout << "+ ";
+                } else {
+                    cout << "- ";
+                }
+                counter++;
+            } else {
+                cout << "+ ";
+            }
         }
         cout << endl;
     } else {

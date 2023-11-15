48b8ded008af6ef12da3a982a97b052ffb1290b0
h268081 <h268081@stud.u-szeged.hu>
2023-11-15 10:25:36 +0100

    minimal bugfixes and added debugging mode

--- flightRoutesCheck/main.cpp
+++ flightRoutesCheck/main.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #define endl "\n"
+#define BUG
 
 using namespace std;
 
@@ -10,14 +11,29 @@ vector<bool> visited;
 vector<bool> invVisited;
 int last;
 
-void BFS(){
+void BFS(bool normal=true){
     while(!q.empty()){
+        last = q.front();
         int node = q.front();
-        visited[node]=true;
+        //normal/inverted switch
+        if(normal) {
+            visited[node] = true;
+        } else {
+            invVisited[node] = true;
+        }
         q.pop();
-        for(auto itr: g[node]){
-            if(!visited[itr]){
-                q.push(itr);
+        //normal/inverted switch
+        if(normal) {
+            for (auto itr: g[node]) {
+                if (!visited[itr]) {
+                    q.push(itr);
+                }
+            }
+        } else {
+            for(auto itr: invg[node]){
+                if(!invVisited[itr]){
+                    q.push(itr);
+                }
             }
         }
     }
@@ -28,7 +44,9 @@ int main() {
     int n,m,a,b;
     cin >> n >> m;
     g.resize(n+1);
+    invg.resize(n+1);
     visited.resize(n+1);
+    invVisited.resize(n+1);
     for(int i=0;i<m;i++){
         cin >> a >>b;
         g[a].insert(b);
@@ -39,12 +57,34 @@ int main() {
     fill(visited.begin(),visited.end(),false);
     q.push(1);
     BFS();
+
     //inverted traverse
     fill(visited.begin(),visited.end(),false);
-    //todo figure out last
     q.push(last);
-    BFS();
-    if(accumulate(visited.begin(),visited.end(),0)!=n && accumulate(invVisited.begin(),invVisited.end(),0)!=n){
+    BFS(false);
+#ifdef BUG
+    cout << "[LOG]last: " << last << endl;
+    cout << "[LOG]visited: " << accumulate(visited.begin(),visited.end(),0) << endl;
+    for(int i=1;i<=n;i++){
+        if(visited[i]){
+            cout << "+";
+        } else {
+            cout << "-";
+        }
+    }
+    cout << endl;
+
+    cout << "[LOG]invVisited: " << accumulate(invVisited.begin(),invVisited.end(),0) << endl;
+    for(int i=1;i<=n;i++){
+        if(invVisited[i]){
+            cout << "+";
+        } else {
+            cout << "-";
+        }
+    }
+    cout << endl;
+#endif
+    if(accumulate(visited.begin(),visited.end(),0)!=n || accumulate(invVisited.begin(),invVisited.end(),0)!=n){
         cout << "NO" << endl;
         //todo print vertices that don't match (also figure out how to do that) or find out the first one that neither visited nor invVisited contains
     } else {
